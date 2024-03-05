86034c5f82c4cc520f071884a3db8419556f1d7e
yi <73318855+Shatterdest@users.noreply.github.com>
2024-03-05 13:16:00 -0500

    display the feed right????

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DataView from '@/views/DataView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: DataView
     },
     {
       path: '/about',
