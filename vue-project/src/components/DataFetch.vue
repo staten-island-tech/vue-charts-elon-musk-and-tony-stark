81fa455ad67b68f69906b5ace14693e830750bb7
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-03-07 13:12:20 -0500

    data!!!!

--- vue-project/src/components/DataFetch.vue
+++ vue-project/src/components/DataFetch.vue
@@ -1,46 +1,49 @@
 <template>
-    <div>  
-      <button @click="fetch">click me</button>
-      </div>
+  <div>
+    <button @click="test">click me</button>
+  </div>
 </template>
 
 <script setup>
-import GtfsRealtimeBindings from "gtfs-realtime-bindings";
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
+import { onBeforeMount } from 'vue'
 let data = []
-async function fetch() {
-  try {
-    const response = await fetch("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm", {
-      headers: {
-        "x-api-key": "Lbc0gTUOwm8GvqZgMmu2t3MEktcvreZr8wiuihJn",
-        // replace with your GTFS-realtime source's auth token
-        // e.g. x-api-key is the header value used for NY's MTA GTFS APIs
-      },
-    });
-    if (!response.ok) {
-      const error = new Error(`${response.url}: ${response.status} ${response.statusText}`);
-      error.response = response;
-      throw error;
-      process.exit(1);
-    }
-    const buffer = await response.arrayBuffer();
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
-      new Uint8Array(buffer)
-    );
-    feed.entity.forEach((entity) => {
-      if (entity.tripUpdate) {
-        console.log(entity.tripUpdate);
-        data = feed
-        console.log(feed)
+async function test() {
+  ;(async () => {
+    try {
+      const response = await fetch(
+        'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm',
+        {
+          headers: {
+            'x-api-key': 'Lbc0gTUOwm8GvqZgMmu2t3MEktcvreZr8wiuihJn'
+            // replace with your GTFS-realtime source's auth token
+            // e.g. x-api-key is the header value used for NY's MTA GTFS APIs
+          }
+        }
+      )
+      if (!response.ok) {
+        const error = new Error(`${response.url}: ${response.status} ${response.statusText}`)
+        error.response = response
+        throw error
+        process.exit(1)
       }
-    });
-  }
-  catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}();
+      const buffer = await response.arrayBuffer()
+      const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer))
+      data = feed.entity
+      console.log(data)
+     
+      
+    } catch (error) {
+      console.log(error)
+      process.exit(1)
+    }
+  })()
+}
+
+onBeforeMount(() => {
+  test()
+  setInterval(test, 5000)
+})
 </script>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
