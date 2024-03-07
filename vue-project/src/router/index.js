7124b1f31f98deeab722eedb8ff04eff09689d32
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-03-07 12:42:50 -0500

    fals;kdfjds

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,20 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DataView from '@/views/DataView.vue'
+import DataFetch from '@/components/DataFetch.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: DataView
-    },
+      component: DataFetch
+        },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      
     }
   ]
 })
