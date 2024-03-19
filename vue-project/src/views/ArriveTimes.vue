e1db9a2a640a9039836bf0851814a076915ee2d9
yi <73318855+Shatterdest@users.noreply.github.com>
2024-03-19 13:12:40 -0400

    holy shit

--- vue-project/src/views/ArriveTimes.vue
+++ vue-project/src/views/ArriveTimes.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <div v-for="item in usedata" :key="item.id">
-      <!-- <h2>{{ routes[item.vehicle.trip.routeId].route_long_name }}</h2>
-      <h2>{{ stops[item.vehicle.stopId].stop_name }}</h2> -->
+    <div v-for="item in data" :key="item.id">
+      <h2>{{ routes[item.vehicle.vehicle.trip.routeId].route_long_name }}</h2>
+      <h2>Currently stopped at: {{ stops[item.vehicle.vehicle.stopId].stop_name }}</h2>
+      
+      <h3 v-for="stop in item.tripUpdate.tripUpdate.stopTimeUpdate" :key="stop.arrival.time"> {{ stop }}</h3>
       <h2>{{ item }}</h2>
     </div>
   </div>
 </template>
 
 <script setup>
-import { usedata } from '@/stores/store'
+import { usedata as data } from '@/stores/store'
 import { stops } from '@/stores/google_transit/stops'
 import { routes } from '@/stores/google_transit/routes';
 </script>
