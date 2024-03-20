05762c17c059f033a80566e5490cceef495375b0
yi <73318855+Shatterdest@users.noreply.github.com>
2024-03-20 13:10:55 -0400

    arrival times gotten

--- vue-project/src/views/ArriveTimes.vue
+++ vue-project/src/views/ArriveTimes.vue
@@ -3,8 +3,10 @@
     <div v-for="item in data" :key="item.id">
       <h2>{{ routes[item.vehicle.vehicle.trip.routeId].route_long_name }}</h2>
       <h2>Currently stopped at: {{ stops[item.vehicle.vehicle.stopId].stop_name }}</h2>
-      
-      <h3 v-for="stop in item.tripUpdate.tripUpdate.stopTimeUpdate" :key="stop.arrival.time"> {{ stop }}</h3>
+
+      <h3 v-for="stop in item.tripUpdate.tripUpdate.stopTimeUpdate" :key="stop.arrival.time">
+        Arrives at {{ stops[stop.stopId].stop_name }} in {{ timeToStop(stop.arrival.time) }} minutes.
+      </h3>
       <h2>{{ item }}</h2>
     </div>
   </div>
@@ -13,7 +15,20 @@
 <script setup>
 import { usedata as data } from '@/stores/store'
 import { stops } from '@/stores/google_transit/stops'
-import { routes } from '@/stores/google_transit/routes';
+import { routes } from '@/stores/google_transit/routes'
+
+const timeToStop = function(time) {
+  let currentTime = Date.now()
+  let currentTimeSeconds = toSeconds(currentTime)
+  console.log(`time inputted ${time} - ${currentTimeSeconds}`)
+  let timeLeftUntilStop = time - currentTimeSeconds
+  console.log(timeLeftUntilStop)
+  return Math.round(timeLeftUntilStop / 60)
+}
+
+const toSeconds = function (digit) { 
+  return Math.round(digit / 1000)
+}
 </script>
 
 <style lang="scss" scoped></style>
