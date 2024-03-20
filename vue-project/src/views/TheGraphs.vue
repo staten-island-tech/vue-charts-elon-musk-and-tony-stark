3f1a572a8747df9b5de107888e3bef6369902389
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-03-20 13:07:31 -0400

    Update TheGraphs.vue

--- vue-project/src/views/TheGraphs.vue
+++ vue-project/src/views/TheGraphs.vue
@@ -1,11 +1,12 @@
 <template>
-    <Bar :data="chartData" />
+    <Bar :data="chartData" :key="key"/>
+    <button @click="reload">Reload????</button>
   </template>
   
   <script>
   import { Bar } from 'vue-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  import { routes} from 'store.js'
+  import { routes } from '@/stores/store'
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   
   export default {
@@ -13,6 +14,7 @@
     components: { Bar },
     data() {
       return {
+        key: 0,
         chartData: {
           labels: [ 'B', 'F', 'D', 'M'],
           datasets: [
@@ -23,6 +25,11 @@
             }
           ]
         }
+      }
+    },
+    methods: {
+      reload() {
+        this.key +++ 1
       }
     }
   }
