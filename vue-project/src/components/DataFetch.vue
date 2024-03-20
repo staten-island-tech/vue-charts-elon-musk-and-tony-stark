1db50bd13baaec4aa93efb64350c874bcb45f0e0
Ben Zanelli <97360492+BenZanelli@users.noreply.github.com>
2024-03-20 12:52:24 -0400

    sfasdf

--- vue-project/src/components/DataFetch.vue
+++ vue-project/src/components/DataFetch.vue
@@ -12,7 +12,7 @@ import { ref } from 'vue';
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
+wowowowowow
 const data = ref([])
 async function test() {
   ;(async () => {
