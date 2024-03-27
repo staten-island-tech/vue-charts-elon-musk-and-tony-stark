bdd6fc677e22c4cca2b81c86becd24b77b239060
yi <73318855+Shatterdest@users.noreply.github.com>
2024-03-27 13:14:45 -0400

    FIRST TRY SORTING

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
 import { RouterView } from 'vue-router'
-import { onBeforeMount } from 'vue'
-import { fetchData } from '@/stores/store';
 
-onBeforeMount(() => {
-  fetchData('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm')
-})
 </script>
 
 <template>
